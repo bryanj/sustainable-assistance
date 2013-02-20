7bba417ea3f6ec5a1c5e8af2efee475037ff46f3
Jo Seunghyun <sky@izz.kr>
2013-02-20 11:23:30 +0900

    Create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SustainableAssistance::Application.config.secret_token = '7708ed556b270cffd031e658ea0d16821979ca2f3d63cd44ff9667b6d38503c5a76a33fff4c0dc832f2fda27243bb15ab35d2d388d04693df85f7ff1e6379717'
