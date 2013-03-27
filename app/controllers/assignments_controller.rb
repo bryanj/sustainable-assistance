b297429c55c0a60d2386de17807f7f17b2817f67
Jo Seunghyun <sky@izz.kr>
2013-03-27 11:49:22 +0900

    Show submission period at assignment introduction page

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,6 +2,7 @@ class AssignmentsController < ApplicationController
   before_filter :set_variables
   def show
     @assignment = Assignment.find(params[:id])
+    @period = Period.where(assignment_id: @assignment.id).where(["end_time > ?", Time.now]).first
   end
 
   def cheating
--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,8 @@
 class Period < ActiveRecord::Base
   attr_accessible :assignment_id, :end_time, :start_time, :result_time
   belongs_to :assignment
+
+  def index
+    self.id % 5
+  end
 end
