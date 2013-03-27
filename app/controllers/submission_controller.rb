f4b55f98a0991c32382ac4182ed38522854982eb
Jo Seunghyun <sky@izz.kr>
2013-03-27 12:04:46 +0900

    Change the order of actions after submission

--- app/controllers/submission_controller.rb
+++ app/controllers/submission_controller.rb
@@ -30,8 +30,8 @@ class SubmissionController < ApplicationController
     submission = Submission.new(assignment_id: params[:assignment_id], period_id: period.id, user_id: session[:user_id])
     submission.file = params[:file]
     if submission.save
-      Stalker.enqueue("submission.run_test", id: submission.id)
       Stalker.enqueue("submission.send_notification", id: submission.id)
+      Stalker.enqueue("submission.run_test", id: submission.id)
       flash[:notice] = "제출되었습니다!"
       redirect_to "/submission/list"
     else
